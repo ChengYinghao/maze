9b966aee74aeedc864bbd6207b5712bcd4d173c5
CYH <1131637423@qq.com>
2019-11-24 15:49:36 +0800

    尝试获取路径和死胡同数量。

--- maze_cyh.py
+++ maze_cyh.py
@@ -6,13 +6,16 @@ class Cell:
         self.right = right
         self.up = up
         self.down = down
+        self.map = {0: self.left, 1: self.right, 2: self.up, 3: self.down}
 
 
 class Maze:
     def __init__(self, input_file):
-        self.cells = []
+        self.cells = None
         self.file = input_file
         self.matrix = None
+        self.rows = 0
+        self.columns = 0
 
     def parse_file(self):
         with open(self.file, 'r') as f:
@@ -27,51 +30,249 @@ class Maze:
                 new_lines = [list(map(int, new_line[0])) for new_line in new_lines]
             matrix = [[int(num) for num in new_line] for new_line in new_lines]
         self.matrix = matrix
+        self.rows = len(self.matrix) - 1
+        self.columns = len(self.matrix[0]) - 1
+        self.cells = [[0 for j in range(self.columns)] for i in range(self.rows)]
 
     def parse_cells(self):
-        row = len(self.matrix) - 1
-        column = len(self.matrix[0]) - 1
-        for i in range(row):
-            for j in range(column):
+        for i in range(self.rows):
+            for j in range(self.columns):
                 left = self.matrix[i][j] == 0 or self.matrix[i][j] == 1
                 up = self.matrix[i][j] == 0 or self.matrix[i][j] == 2
                 right = self.matrix[i][j + 1] == 0 or self.matrix[i][j + 1] == 1
                 down = self.matrix[i + 1][j] == 0 or self.matrix[i + 1][j] == 2
                 cell = Cell(i, j, left, right, up, down)
-                self.cells.append(cell)
+                self.cells[i][j] = cell
 
     def getNumOfGates(self):
         num = 0
-        for cell in self.cells:
-            if cell.x == 0 and cell.y == 0:
-                num += int(cell.left) + int(cell.up)
-                continue
-            if cell.x == 0 and cell.y == len(self.matrix[0]) - 2:
-                num += int(cell.up) + int(cell.right)
-                continue
-            if cell.x == len(self.matrix) - 2 and cell.y == 0:
-                num += int(cell.left) + int(cell.down)
-                continue
-            if cell.x == len(self.matrix) - 2 and cell.y == len(self.matrix[0]) - 2:
-                num += int(cell.right) + int(cell.down)
-                continue
-            if cell.x == 0:
-                num += int(cell.up)
-                continue
-            if cell.x == len(self.matrix) - 2:
-                num += int(cell.down)
-                continue
-            if cell.y == 0:
+        for x in range(self.rows):
+            for y in range(self.columns):
+                cell = self.cells[x][y]
+                if x == 0 and y == 0:
+                    num += int(cell.left) + int(cell.up)
+                    continue
+                if x == 0 and y == self.columns - 1:
+                    num += int(cell.up) + int(cell.right)
+                    continue
+                if x == self.rows - 1 and y == 0:
+                    num += int(cell.left) + int(cell.down)
+                    continue
+                if x == self.rows - 1 and y == self.columns - 1:
+                    num += int(cell.right) + int(cell.down)
+                    continue
+                if x == 0:
+                    num += int(cell.up)
+                    continue
+                if x == self.rows - 1:
+                    num += int(cell.down)
+                    continue
+                if y == 0:
+                    num += int(cell.left)
+                    continue
+                if y == self.columns - 1:
+                    num += int(cell.right)
+                    continue
+            return num
+
+    def traverse(self):
+        initial = [[0 for j in range(self.columns)] for i in range(self.rows)]
+        color = 0
+        cul_de_sac = 0
+        for x in range(self.rows):
+            for y in range(self.columns):
+                cell = self.cells[x][y]
+                if (x == 0 and y == 0) and (cell.left or cell.up) and initial[x][y] == 0:
+                    color += 1
+                    self.dfs(cell, initial, color)
+                    continue
+                if (x == 0 and y == self.columns - 1) and (cell.right or cell.up) and initial[x][
+                    y] == 0:
+                    color += 1
+                    self.dfs(cell, initial, color)
+                    continue
+                if (x == self.rows - 1 and y == 0) and (cell.left or cell.down) and initial[x][
+                    y] == 0:
+                    color += 1
+                    self.dfs(cell, initial, color)
+                    continue
+                if (x == self.rows - 1 and y == self.columns - 1) and (cell.right or cell.down) and \
+                        initial[x][y] == 0:
+                    color += 1
+                    self.dfs(cell, initial, color)
+                    continue
+                if x == 0 and cell.up and initial[x][y] == 0:
+                    color += 1
+                    self.dfs(cell, initial, color)
+                    continue
+                if x == self.rows - 1 and cell.down and initial[x][y] == 0:
+                    color += 1
+                    self.dfs(cell, initial, color)
+                    continue
+                if y == 0 and cell.left and initial[x][y] == 0:
+                    color += 1
+                    self.dfs(cell, initial, color)
+                    continue
+                if y == self.columns - 1 and cell.right and initial[x][y] == 0:
+                    color += 1
+                    self.dfs(cell, initial, color)
+                    continue
+        return (initial, color)
+
+    def dfs(self, startCell, initial, color):
+        x = startCell.x
+        y = startCell.y
+        initial[x][y] = color
+        if x == 0:
+            if y != self.columns - 1:
+                if startCell.right and initial[x][y + 1] == 0:
+                    initial[x][y + 1] = color
+                    self.dfs(self.cells[x][y + 1], initial, color)
+            if startCell.down and initial[x + 1][y] == 0:
+                initial[x + 1][y] = color
+                self.dfs(self.cells[x + 1][y], initial, color)
+            if y != 0:
+                if startCell.left and initial[x][y - 1] == 0:
+                    initial[x][y - 1] = color
+                    self.dfs(self.cells[x][y - 1], initial, color)
+
+        elif x == self.rows - 1:
+            if startCell.up and initial[x - 1][y] == 0:
+                initial[x - 1][y] = color
+                self.dfs(self.cells[x - 1][y], initial, color)
+            if y != self.columns - 1:
+                if startCell.right and initial[x][y + 1] == 0:
+                    initial[x][y + 1] = color
+                    self.dfs(self.cells[x][y + 1], initial, color)
+            if y != 0:
+                if startCell.left and initial[x][y - 1] == 0:
+                    initial[x][y - 1] = color
+                    self.dfs(self.cells[x][y - 1], initial, color)
+        elif y == 0:
+            if startCell.right and initial[x][y + 1] == 0:
+                initial[x][y + 1] = color
+                self.dfs(self.cells[x][y + 1], initial, color)
+            if x != 0:
+                if startCell.up and initial[x - 1][y] == 0:
+                    initial[x - 1][y] = color
+                    self.dfs(self.cells[x - 1][y], initial, color)
+            if x != self.rows - 1:
+                if startCell.down and initial[x + 1][y] == 0:
+                    initial[x + 1][y] = color
+                    self.dfs(self.cells[x + 1][y], initial, color)
+        elif y == self.columns - 1:
+            if startCell.left and initial[x][y - 1] == 0:
+                initial[x][y - 1] = color
+                self.dfs(self.cells[x][y - 1], initial, color)
+            if x != 0:
+                if startCell.up and initial[x - 1][y] == 0:
+                    initial[x - 1][y] = color
+                    self.dfs(self.cells[x - 1][y], initial, color)
+            if x != self.rows - 1:
+                if startCell.down and initial[x + 1][y] == 0:
+                    initial[x + 1][y] = color
+                    self.dfs(self.cells[x + 1][y], initial, color)
+        else:
+            if startCell.left and initial[x][y - 1] == 0:
+                initial[x][y - 1] = color
+                self.dfs(self.cells[x][y - 1], initial, color)
+            if startCell.right and initial[x][y + 1] == 0:
+                initial[x][y + 1] = color
+                self.dfs(self.cells[x][y + 1], initial, color)
+            if startCell.up and initial[x - 1][y] == 0:
+                initial[x - 1][y] = color
+                self.dfs(self.cells[x - 1][y], initial, color)
+            if startCell.down and initial[x + 1][y] == 0:
+                initial[x + 1][y] = color
+                self.dfs(self.cells[x + 1][y], initial, color)
+
+    # 给定边界坐标，判断是否能出迷宫, 并返回能出的通道数
+    def is_out(self, x, y):
+        num = 0
+        cell = self.cells[x][y]
+        if x == 0:
+            if cell.up:
+                num += 1
+            if y == 0:
                 num += int(cell.left)
-                continue
-            if cell.y == len(self.matrix[0]) - 2:
+            if y == self.columns - 1:
                 num += int(cell.right)
-                continue
+        elif x == self.rows - 1:
+            if cell.down:
+                num += 1
+            if y == 0:
+                num += int(cell.left)
+            if y == self.columns - 1:
+                num += int(cell.right)
+        elif y == 0:
+            num += int(cell.left)
+        elif y == self.columns - 1:
+            num += cell.right
         return num
 
+    def getGates(self, x0, y0, initial, edges_points):
+        gates = []
+        for point in edges_points:
+            x = point[0]
+            y = point[1]
+            if x != x0 & y != y0 and self.is_out(x, y) and initial[
+                x][y] == initial[x0][y0]:
+                gates.append(point)
+        return gates
 
-maze = Maze('.\\a2_sanity_check\\labyrinth.txt')
+    def getOtherPoints(self, x0, y0, initial):
+        num = 0
+        for x in range(self.rows):
+            for y in range(self.columns):
+                if (x != x0 or y != y0) and initial[x][y] == initial[x0][y0]:
+                    num += 1
+        return num
+
+    def getPaths(self, initial):
+        paths = 0
+        cul_de_sacs = 0
+        edges_points = [[(0, y1) for y1 in range(self.columns)] + [(self.rows - 1, y2) for y2 in range(self.columns)]
+                        + [(x1, 0) for x1 in range(1, self.rows - 1)] + [(x2, self.columns - 1) for x2 in
+                                                                         range(1, self.rows - 1)]][0]
+        visited = [[0 for j in range(self.columns)] for i in range(self.rows)]
+        for point in edges_points:
+            x = point[0]
+            y = point[1]
+            visited[x][y] = 1
+            outs = self.is_out(x, y)
+            if initial[x][y] == 0:
+                continue
+            if outs == 0:
+                cul_de_sacs += 1
+                continue
+            if outs > 1:
+                paths += 1
+            gates = self.getGates(x, y, initial, edges_points)
+            if len(gates) == 0:
+                if self.getOtherPoints(x, y, initial):
+                    cul_de_sacs += 1
+                continue
+            if len(gates) == 1:
+                paths += 1
+                otherPoints = self.getOtherPoints(x, y, initial)
+
+                if self.getOtherPoints(x, y, initial) > 1:
+                    cul_de_sacs += 1
+                continue
+        return (paths, cul_de_sacs)
+
+
+maze = Maze('.\\a2_sanity_check\\maze_2.txt')
 maze.parse_file()
 print(maze.matrix)
 maze.parse_cells()
-print(maze.getNumOfGates())
+# print(maze.getNumOfInaccessiblePoints())
+initial, color = maze.traverse()
+
+for line in initial:
+    print(line)
+print(sum([sum([int(num == 0) for num in line]) for line in initial]))
+print(color)
+paths, cul_de_sacs = maze.getPaths(initial)
+print("paths: ", paths)
+print("cul_de_sacs: ", cul_de_sacs)
